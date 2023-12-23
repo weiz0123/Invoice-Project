42e32055d8567ac7f78a987257c780b8e384fb4a
weiz0123 <76544381+weiz0123@users.noreply.github.com>
2023-12-23 16:18:44 -0700

    Update Data_Access.py

--- PIV/Access/Data_Access.py
+++ PIV/Access/Data_Access.py
@@ -28,7 +28,7 @@ class Data_Access:
         cls.data_access_root_path = data_access_root_path
 
         # this list represents a model of companies. each element represents a "company manager" for the target company
-        cls.class_level_management_list = []
+        cls.class_level_management_list: list = []
 
         # search and add all the company_manager for all company directories
         cls._generate_company_level_management(cls)
@@ -111,15 +111,61 @@ class Data_Access:
         param (target_company_name: str, target_img_name: str)
         return ndarray that represents image
         """
+
         company_manager: Company_Level_Management = self._get_company_level_management(target_company_name)
         return company_manager.get_image_name_list()
 
-    def get_category_list(self) -> list[str]:
+    def get_category_list(self, company_name: str) -> list[str]:
         """
         * further design, documentation will be included for this method.
         """
-        tempList = ["invoice number", "invoice date"]
-        return tempList
+        target_company_manager: Company_Level_Management = self._get_company_level_management(company_name)
+        category_list: list[str] = target_company_manager.get_category_list()
+        return category_list
 
+    def get_extracted_data_for_all_image(self, company_name: str) -> dict[dict]:
+        target_company_manager: Company_Level_Management = self._get_company_level_management(company_name)
+        extracted_data: dict[dict] = target_company_manager.get_extracted_data()
+        return extracted_data
 
+    def get_extracted_data_for_targeted_image(self, company_name: str, image_name: str):
+        extracted_data: dict[dict] = self.get_extracted_data_for_all_image(company_name)
+        for image in extracted_data.keys():
+            if image == image_name:
+                return extracted_data[image]
 
+        raise Exception("Data_Access: get_extracted_data_for_target_image() -> No image found")
+
+    def get_position_for_all_category(self, company_name: str) -> dict:
+        target_company_manager: Company_Level_Management = self._get_company_level_management(company_name)
+        position_data: dict = target_company_manager.get_position_list()
+        return position_data
+
+    def get_position_for_target_category(self, company_name: str, category_key: str):
+        position_data: dict = self.get_position_for_all_category(company_name)
+        for category in position_data.keys():
+            if category == category_key:
+                return position_data[category]
+
+        raise Exception("Data_Access: get_position_for_target_category() -> no position found")
+
+    def update_data(self, company_name: str, command: str, image_name: str, category_key: str = None, text: str = None):
+        target_company_manager: Company_Level_Management = self._get_company_level_management(company_name)
+        target_company_manager.update_data_info(command, image_name, category_key, text)
+
+    def update_position(self, company_name: str, command: str, category_key: str = None, pos1: list[float] = None,
+                        pos2: list[float] = None):
+        target_company_manager: Company_Level_Management = self._get_company_level_management(company_name)
+        target_company_manager.update_position_info(command, category_key, pos1, pos2)
+
+    def update_category(self, company_name: str, category_list: list[str]):
+        target_company_manager: Company_Level_Management = self._get_company_level_management(company_name)
+        target_company_manager.update_category(category_list)
+
+    def update_to_file(self, company_name: str):
+        target_company_manager: Company_Level_Management = self._get_company_level_management(company_name)
+        target_company_manager.update_to_file()
+
+    def get_company_path(self, company_name: str):
+        target_company_manager: Company_Level_Management = self._get_company_level_management(company_name)
+        return target_company_manager.get_company_path()
