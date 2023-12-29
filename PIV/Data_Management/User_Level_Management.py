6f6922d71db1fbd0b4c55c3d44e6cb8d05aa8a3c
weiz0123 <76544381+weiz0123@users.noreply.github.com>
2023-12-28 23:32:04 -0700

    Update User_Level_Management.py

--- PIV/Data_Management/User_Level_Management.py
+++ PIV/Data_Management/User_Level_Management.py
@@ -2,7 +2,7 @@ from numpy import ndarray
 
 from Access.Data_Access import Data_Access
 from Access.Process_Access import Process_Access
-
+from Access.Update_Access import Update_Access
 # This path is for testing purpose
 DEFAULT_PATH = "C:\\Users\\zhouw\\OneDrive\\Documents\\personal sci project\\vs\\ProjectIV\\Test_Resource\\company_test"
 
@@ -28,6 +28,7 @@ class User_Level_Management:
         self.root_path: str = root_path
         self.data_access: Data_Access = Data_Access(root_path)
         self.process_access: Process_Access = Process_Access(root_path)
+        self.update_access: Update_Access = Update_Access(self.data_access, self)
 
     # The following controls are from data_access
     def display_company_name(self) -> list[str]:
@@ -59,7 +60,7 @@ class User_Level_Management:
             raise Exception("fail to display cv image")
         return self.data_access.get_company_targeted_image(company_name, first_img_name)
 
-    def display_image_name(self, company_name: str) -> list[ndarray]:
+    def display_image_name(self, company_name: str) -> list[str]:
         """
         1. if the image list has length of zero, then it raise exception but not handled at the time
             * further design and refine is required for this exception
@@ -72,6 +73,25 @@ class User_Level_Management:
             return img_name_list
         else:
             raise Exception("fail to display image name from User_Level_Management")
+
+    def display_image_position_info(self, company_name: str) -> dict:
+        return self.data_access.get_position_for_all_category(company_name)
+
+    def display_extracted_information_for_target_image(self, company_name: str, image_name: str) -> dict[dict]:
+        return self.data_access.get_extracted_data_for_targeted_image(company_name, image_name)
+
+    def update_data(self, company_name: str, command: str, image_name: str, category_key: str = None, text: str = None):
+        self.data_access.update_data(company_name, command, image_name, category_key, text)
+
+    def update_position(self, company_name: str, command: str, category_key: str = None, pos1: list[float] = None,
+                        pos2: list[float] = None):
+        self.data_access.update_position(company_name, command, category_key, pos1, pos2)
+
+    def update_category(self, company_name: str, category_list: list[str]):
+        self.data_access.update_category(company_name, category_list)
+
+    def update_to_file(self, company_name: str):
+        self.update_to_file(company_name)
 
     # The following controls are from Process Access
     # {"invoice number": [[1337.7434768516018, 130.97587920695787], [1491.2558028559597, 202.7800316928674]],
@@ -96,32 +116,24 @@ class User_Level_Management:
         return truth value, if converted successfully, return ture, otherwise false
         """
         try:
-            self.process_access.convert_existing_pdf(company_name, pdf_path)
+            self.process_access.convert_existing_pdf(company_name, [pdf_path])
             return True
         except ValueError(f"unable to convert {pdf_path} to jpg"):
             return False
+"""
+def main():
+    USER = User_Level_Management(DEFAULT_PATH)
+    running = True
+    while running:
+        print("===================================functino test and demonstration=====================================")
+        print("Choose a function by the number")
+        print("1. display_company_name")
+        print("2. display_extraction_category")
+        print("3. display_first_image")
+        print("4. display_image_name_list")
 
-    def display_image_position_info(self, company_name: str) -> dict:
-        return self.data_access.get_position_for_all_category(company_name)
+        number: int = int(input("enter function number: "))
+  
 
-    def display_extracted_information_for_target_image(self, company_name: str, image_name: str) -> dict[dict]:
-        return self.data_access.get_extracted_data_for_targeted_image(company_name, image_name)
-
-    def update_data(self, company_name: str, command: str, image_name: str, category_key: str = None, text: str = None):
-        self.data_access.update_data(company_name, command,image_name,category_key,text)
-
-    def update_position(self, company_name: str, command: str, category_key: str = None, pos1: list[float] = None,
-                        pos2: list[float] = None):
-        self.data_access.update_position(company_name, command,category_key,pos1,pos2)
-
-    def update_category(self, company_name: str, category_list: list[str]):
-        self.data_access.update_category(company_name, category_list)
-
-    def update_to_file(self, company_name: str):
-        self.update_to_file(company_name)
-
-
-
-
-
-
+main()
+"""
