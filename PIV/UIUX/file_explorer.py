d3e4df090bb3f0904c9ce89975aa195e55329d6c
SaveQaQ <98987577+SaveQaQ@users.noreply.github.com>
2023-12-31 22:31:20 -0700

    Add files via upload
    draging files to folder is almost done. Considered improvement: 1.draging to different floder (now can only be draged to the root folder, can not copy to sub-folder directly yet), 2. drag to default company folder may be a better choice, need discussion

--- PIV/UIUX/file_explorer.py
+++ PIV/UIUX/file_explorer.py
@@ -1,5 +1,8 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTreeWidget, QTreeWidgetItem
 from PyQt5.QtCore import QDir, Qt, QUrl
+import os
+
+path = os.getcwd()
 class my_tree(QTreeWidget):
     def __int__(self, parent=None):
         super().__int__(parent)
@@ -22,8 +25,21 @@ class my_tree(QTreeWidget):
         if event.mimeData().hasUrls():
             event.setDropAction(Qt.CopyAction)
             event.accept()
-            print(event.mimeData().urls())
+            for url in event.mimeData().urls():
+                if url.isLocalFile():
+                    print(str(url))
 
+                    file_path = url.path()
+                    file_path = file_path[1:]
+                    file_path = r'' + file_path
+                    folder_path = r''+path.replace("\\","/")      #replace path C:\... to C:/ so that we can use copy to copy file to folders
+                    print(file_path, folder_path)
+                    import shutil
+                    try:
+                        shutil.copy(file_path, folder_path)
+                        print("File copied successfully")
+                    except Exception as e:
+                        print(f"Fail to copy\nAn error occurred: {e}")
 
         else:
             event.ignore()
