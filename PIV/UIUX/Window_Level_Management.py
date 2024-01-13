a203b8a659684b0406a86ca24c3327c86ad10d48
SaveQaQ <98987577+SaveQaQ@users.noreply.github.com>
2024-01-13 15:50:19 -0700

    Add files via upload
    able to adjust rectangle when maximizing, but resizing is stiill in progross

--- PIV/UIUX/Window_Level_Management.py
+++ PIV/UIUX/Window_Level_Management.py
@@ -1,9 +1,12 @@
 from v2 import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import Qt, QDir, QUrl
+from PyQt5.QtCore import Qt, QDir, QUrl, QRect
 from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem, QCompleter, QDialog, QFileDialog, QLabel
+from PyQt5.QtGui import QPixmap, QPainter, QColor
 from file_explorer import my_tree
 from Image_Level_Management import Image_Level_Management
+from Painter_Level_Management import Painter
+
 import image_clicked_recorder
 import category_dialog
 import subprocess
@@ -12,29 +15,38 @@ import sys
 alist = ['1', '2']  # for testing
 
 
-class ExtendedUi_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+class ExtendedUi_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):  # QtWidgets.QWidget
     def __init__(self, path):
         super().__init__()
+        self.image_label = Painter()
+        self.isDrawing = False
         self.window_size = [702, 475]
+        self.start_point = QtCore.QPoint(0, 0)
+        self.end_point = QtCore.QPoint(0, 0)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.initialization(path)
 
     def initialization(self, path):
-        self.init_label()
+
         self.initialize_file_explorer(path)
         self.update_company_combo_box(path)
-        self.initialize_search_box()
+        self.update_search_box()
         self.update_category_combo_box()
         self.triggered_tools(path)
         self.update_status(path)
+        self.init_label(path)
+        # self.ui.tab = Painter()
+        # self.painter = Painter()
+        # self.ui.Matplot_VLayout.addWidget(self.painter)
 
     def resizeEvent(self, event):
-        if self.count_resize > 3:
+        if self.count_resize > 2:
             # This method will be called whenever the main window is resized
+            old_window_size = [self.window_size[0], self.window_size[1]]
             self.window_size[0] = event.size().height()
             self.window_size[1] = event.size().width()
-            # You can add your custom logic here based on the new size of the window
+
             max_label_width = self.window_size[1] // 2
             max_label_height = max_label_width * 2200 // 1700
             if max_label_width > 950:
@@ -43,20 +55,29 @@ class ExtendedUi_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.image_label.setMaximumSize(max_label_width, max_label_height)
 
             self.image_label.resize(QtCore.QSize(max_label_width, max_label_height))
-            # self.ui.Matplot_VLayout.setContentsMargins(self.window_size[0] // 10, 0, self.window_size[0] // 10, 0)
+            #self.image_label_set_x_y
+
             self.count_resize += 1
-            print(
-                f"Window resized to: {self.window_size}, max width: {max_label_width}, max height: {max_label_height}")
+            #print(
+            #    f"Window resized to: {self.window_size}, max width: {max_label_width}, max height: {max_label_height}")
         else:
             self.count_resize += 1
 
-    def init_label(self):
-        image_path = r"C:\Users\zhiji\Documents\company\CompanyI\Images\Invoice_000000000664731.jpg"
+    def init_label(self, path):
+        folder_path = path + "\company\\"
+        company = self.ui.Company_ComboBox.currentText()
+        company_path = folder_path + company + "\Images"
+        entries = self.get_entries(company_path)
+        first_entry = entries[0]
+        for entry in entries:
+            if entry.fileName() not in ('.', '..'):
+                first_entry = entry
+                break
+        image_path = company_path + "\\" + first_entry.fileName()
         image_object = Image_Level_Management(image_path)
         image = image_object.get_cv_img()
         self.count_resize = 1
         if image is not None:
-            self.image_label = QLabel()
             self.ui.Matplot_VLayout.setContentsMargins(self.window_size[0] // 10, 0, self.window_size[0] // 10, 0)
             max_label_width = self.window_size[1]
             max_label_height = max_label_width * 2200 // 1700
@@ -73,7 +94,8 @@ class ExtendedUi_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.File_Explore_List_Widget.setObjectName("File_Explore_List_Widget")
         self.ui.File_Explore_List_Widget.headerItem().setText(0, "Invoice-Project")
         self.ui.File_Explorer_VLayout.addWidget(self.ui.File_Explore_List_Widget)
-        self.ui.File_Explore_List_Widget.populate_tree(path)
+        company_path = path + "/company"
+        self.ui.File_Explore_List_Widget.populate_tree(company_path)
 
     def get_entries(self, path):
         dir = QDir(path)
@@ -94,10 +116,10 @@ class ExtendedUi_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.ui.Company_ComboBox.removeItem(0)
         self.update_company_combo_box(path)
 
-    def initialize_search_box(self):
-        target_list = ['apple', 'abandon', 'abnormal', 'orange']
+    def update_search_box(self):
+        self.target_list = [self.ui.Company_ComboBox.itemText(i) for i in range(self.ui.Company_ComboBox.count())]
         # remember to replace target_list to USER.display_compnay_name()
-        completer = QCompleter(target_list, self.ui.search_company_name_lineBox)
+        completer = QCompleter(self.target_list, self.ui.search_company_name_lineBox)
         completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)  # Set case sensitivity
         self.ui.search_company_name_lineBox.setCompleter(completer)
 
@@ -144,6 +166,7 @@ class ExtendedUi_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.actionAdd_category.triggered.connect(self.add_category)
         self.ui.actionOpenCompanyFolder.triggered.connect(lambda: self.open_company_folder(path))
         self.ui.actionrefresh.triggered.connect(lambda: self.refresh(path))
+        self.ui.actionselection_box.triggered.connect(self.draw_rectangle)
 
     def add_category(self):
         Dialog = QtWidgets.QDialog()
@@ -204,8 +227,38 @@ class ExtendedUi_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.File_Explore_List_Widget.populate_tree(path)
         self.clear_update_status_bar(path)
         self.clear_update_company_combo_box(path)
+        self.update_search_box()
         # self.clear_image()
         # self.display_image(path)
 
-    # def clear_image(self):
-    #     self.ui.Matplot_VLayout.removeWidget(self.ui.image_label)
+    def draw_rectangle(self):
+        self.isDrawing = True
+        self.image_label.setDrawing(self.isDrawing)
+
+    # def mousePressEvent(self, event):
+    #     x = event.pos().x()
+    #     y = event.pos().y()
+    #     print(f"Clicked at: ({x}, {y})")
+    #     self.start_point = event.pos()
+    #
+    #
+    # def mouseMoveEvent(self, event):
+    #     self.end_point = event.pos()
+    #     self.update()
+    #
+    # def mouseReleaseEvent(self, event):
+    #     x = event.pos().x()
+    #     y = event.pos().y()
+    #     print(f"release at: ({x}, {y})")
+    #     self.end_point = event.pos()
+    #
+    # def paintEvent(self, event):
+    #     painter = QPainter(self)
+    #     painter.setPen(QColor(0, 0, 255))  # Blue color for the rectangle outline
+    #
+    #     if self.start_point is not None and self.end_point is not None:
+    #         # Calculate the rectangle coordinates using start and end points
+    #         rect = QRect(self.start_point, self.end_point)
+    #         painter.drawRect(rect)
+    #
+    #
